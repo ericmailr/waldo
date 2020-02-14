657eb400ce2c6df1634c579c4948aaa826c50b63
Eric Miller <ericmailr@gmail.com>
2020-02-13 19:39:12 -0700

    finish front-end functionality

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -16,40 +16,43 @@ const init = () => {
     const toggleKeyLabelEl = document.getElementById('toggleKeyLabel');
     const sceneOptionEl = document.getElementById('sceneOption');
     const menuScenesEl = document.getElementById('menuScenes');
-    const circleEl = document.getElementById('circle');
+    const spottedEl = document.getElementById('spotted');
+    const identifyEl = document.getElementById('identify');
+    const charactersEl = document.getElementById('characters');
 
     containerEl.style.paddingTop = keyLabelEl.clientHeight + 'px';
     keyEl.style.display = 'none';
     menuScenesEl.style.display = 'none';
 
+    let currentX = 0;
+    let currentY = 0;
+
     const scene1El = document.getElementById('scene1');
-    //
+    //waldo: 2555 1385
+    //wenda: 3040 475
+    //woof: 1326 1641
+    //odlaw: 3266 1250
+    //wizard whitebeard: 359 1471
+    //scroll: 2908 2071
+    //camera: 780 1481
+    //key: 1101 1478
+    //binoculars: 1538 1933
+    //bone: 2652 974
     const scene2El = document.getElementById('scene2');
     //waldo: 2100 665
     const scene3El = document.getElementById('scene3');
     //
 
-    const setContainerDimensions = () => {
-        containerEl.style.width = sceneEl.clientWidth + 'px';
-        containerEl.style.height = sceneEl.clientHeight + 'px';
-        console.log(containerEl.style.width);
-    }
-
-    setContainerDimensions();
-
     scene1El.addEventListener('click', () => {
         sceneEl.src = 'assets/waldo1.jpg';
-        setContainerDimensions();
     })
 
     scene2El.addEventListener('click', () => {
         sceneEl.src = 'assets/waldo2.jpg';
-        setContainerDimensions();
     })
 
     scene3El.addEventListener('click', () => {
         sceneEl.src = 'assets/waldo3.jpg';
-        setContainerDimensions();
     })
 
     const toggleKeyHandler = () => {
@@ -72,10 +75,27 @@ const init = () => {
 
     sceneOptionEl.addEventListener('click', toggleScenesHandler)
     keyOptionEl.addEventListener('click', toggleKeyHandler) 
-    sceneEl.addEventListener('click', (e) => {
-        circleEl.style.display = 'block';
-        circleEl.style.top = e.pageY - 55 + 'px';
-        circleEl.style.left = e.pageX - 55 + 'px';
-
+    containerEl.addEventListener('click', (e) => {
+        spottedEl.style.display = 'flex';
+        spottedEl.style.top = e.pageY - 55 + 'px';
+        spottedEl.style.left = e.pageX - 55 + 'px';
+        identifyEl.style.display = 'block';
+        charactersEl.style.display = 'none';
     })
+
+    identifyEl.addEventListener('click', (e) => {
+        e.stopPropagation();
+        identifyEl.style.display = 'none';
+        charactersEl.style.display = 'block';
+        console.log('it worked');
+    })
+
+    charactersEl.addEventListener('click', (e) => {
+        e.stopPropagation();
+        identifyEl.style.display = 'block';
+        charactersEl.style.display = 'none';
+        console.log(e.target.innerHTML);
+        // is it within 30px of e.target.innerHTML
+    })
+
 }
